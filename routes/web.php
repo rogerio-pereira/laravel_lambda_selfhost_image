b9be7e1bf809d87ee432b4338de004efa6538a35
Rogerio Pereira <hi@rogeriopereira.dev>
2024-10-07 15:15:22 -0400

    Change / route to return json

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,11 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return response()->json([
+        'status' => 'laravel ok',
+        'message' => 'This is a message from api',
+    ], 200);
 });
 
 Route::get('/healthcheck', function () {
